174b093327f2b0f8833e1d424a4004b65109314f
DennisKretz <unrealfaceit2020@gmail.com>
2023-01-22 04:19:22 +0100

    add scraper

---
+++ main/client/scraper/main.py
@@ -0,0 +1,41 @@
+from abc import ABCMeta, abstractstaticmethod
+from bs4 import BeautifulSoup
+
+class IScraperException(Exception):
+    pass
+
+class IScraper(metaclass=ABCMeta):
+
+    @abstractstaticmethod
+    def scraper_method():
+        """ Interface Method"""
+
+    @abstractstaticmethod
+    def by_class():
+        """ Interface Method """
+
+    @abstractstaticmethod
+    def by_id():
+        """ Interface Method """
+
+class Find(IScraper):
+
+    def __init__(self, element: str):
+        self._element = element
+
+    def scraper_method(self, request_content: str):
+        return BeautifulSoup(request_content, 'html.parser')
+
+    def by_class(self, soup: BeautifulSoup):
+        return soup.find(_class=self._element)
+
+    def by_id(self, soup: BeautifulSoup):
+        return soup.find(id=self._element)
+
+class ScraperFactory:
+    
+    @staticmethod
+    def find_data(data_type: int, element: str):
+        if (data_type == 0):
+            return Find(element=element)
+        raise IScraperException("Invalid data type")
